0a51703271671a8babe61fd481ffee3a26fa081d
Matt Schuchard <grapheneowns@gmail.com>
2016-04-28 07:28:46 -0400

    adding support for warning and deprecation warning captures

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Linter-Ansible-Syntax aims to provide functional and robust Ansible syntax check
 Ansible is required to be installed (preferably from a package or a pip) before using this.  The 'linter' and 'language-ansible' atom packages are also required but should be automatically installed as dependencies thanks to steelbrain's package-deps.
 
 ### Usage
-- The `ansible-playbook --syntax-check` output sometimes does not output line and column information. When it guesses the line and column then that information will be displayed. If it does not, the error will be displayed for line 1, column 1.
+- The `ansible-playbook --syntax-check` output sometimes does not output line and column information for errors. When it guesses the line and column, then that information will be displayed. If it does not, the error will be displayed at line 1, column 1.
+- The first warning and deprecation warning will be displayed at line 1, column 1. There is never line and column information for these.
 - The Ansible syntax check only outputs the first error it encounters and therefore only the first error in a playbook will be displayed.
--- spec/linter-ansible-syntax-spec.js
+++ spec/linter-ansible-syntax-spec.js
@@ -14,7 +14,6 @@ describe('The Ansible Syntax Check provider for Linter', () => {
       );
     });
   });
-  //TODO: update test and add test_three
 
   describe('checks a file with multiple issues and', () => {
     let editor = null;
@@ -41,13 +40,90 @@ describe('The Ansible Syntax Check provider for Linter', () => {
           expect(messages[0].type).toBeDefined();
           expect(messages[0].type).toEqual('Error');
           expect(messages[0].text).toBeDefined();
-          expect(messages[0].text).toEqual('playbook entries must be either a valid play or an include statement');
+          expect(messages[0].text).toEqual("this task 'hg' has extra params, which is only allowed in the following modules: command, shell, script, include, include_vars, add_host, group_by, set_fact, raw, meta");
           expect(messages[0].filePath).toBeDefined();
           expect(messages[0].filePath).toMatch(/.+test\.yml$/);
           expect(messages[0].range).toBeDefined();
           expect(messages[0].range.length).toBeDefined();
           expect(messages[0].range.length).toEqual(2);
           expect(messages[0].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[1].type).toBeDefined();
+          expect(messages[1].type).toEqual('Warning');
+          expect(messages[1].text).toBeDefined();
+          expect(messages[1].text).toEqual('provided hosts list is empty, only localhost is available');
+          expect(messages[1].filePath).toBeDefined();
+          expect(messages[1].filePath).toMatch(/.+test\.yml$/);
+          expect(messages[1].range).toBeDefined();
+          expect(messages[1].range.length).toBeDefined();
+          expect(messages[1].range.length).toEqual(2);
+          expect(messages[1].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[2].type).toBeDefined();
+          expect(messages[2].type).toEqual('Warning');
+          expect(messages[2].text).toBeDefined();
+          expect(messages[2].text).toEqual('Instead of sudo/sudo_user, use become/become_user and');
+          expect(messages[2].filePath).toBeDefined();
+          expect(messages[2].filePath).toMatch(/.+test\.yml$/);
+          expect(messages[2].range).toBeDefined();
+          expect(messages[2].range.length).toBeDefined();
+          expect(messages[2].range.length).toEqual(2);
+          expect(messages[2].range).toEqual([[0, 0], [0, 32]]);
+        });
+      });
+    });
+  });
+
+  describe('checks a file with multiple issues and', () => {
+    let editor = null;
+    const badFile = path.join(__dirname, 'fixtures', 'test_three.yml');
+    beforeEach(() => {
+      waitsForPromise(() =>
+        atom.workspace.open(badFile).then(openEditor => {
+          editor = openEditor;
+        })
+      );
+    });
+
+    it('finds at least one message', () => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
+          expect(messages.length).toBeGreaterThan(0);
+        })
+      );
+    });
+
+    it('verifies the messages', () => {
+      waitsForPromise(() => {
+        return lint(editor).then(messages => {
+          expect(messages[0].type).toBeDefined();
+          expect(messages[0].type).toEqual('Error');
+          expect(messages[0].text).toBeDefined();
+          expect(messages[0].text).toEqual('error while splitting arguments, either an unbalanced jinja2 block or quotes');
+          expect(messages[0].filePath).toBeDefined();
+          expect(messages[0].filePath).toMatch(/.+test\.yml$/);
+          expect(messages[0].range).toBeDefined();
+          expect(messages[0].range.length).toBeDefined();
+          expect(messages[0].range.length).toEqual(2);
+          expect(messages[0].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[1].type).toBeDefined();
+          expect(messages[1].type).toEqual('Warning');
+          expect(messages[1].text).toBeDefined();
+          expect(messages[1].text).toEqual('provided hosts list is empty, only localhost is available');
+          expect(messages[1].filePath).toBeDefined();
+          expect(messages[1].filePath).toMatch(/.+test\.yml$/);
+          expect(messages[1].range).toBeDefined();
+          expect(messages[1].range.length).toBeDefined();
+          expect(messages[1].range.length).toEqual(2);
+          expect(messages[1].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[2].type).toBeDefined();
+          expect(messages[2].type).toEqual('Warning');
+          expect(messages[2].text).toBeDefined();
+          expect(messages[2].text).toEqual('Instead of sudo/sudo_user, use become/become_user and');
+          expect(messages[2].filePath).toBeDefined();
+          expect(messages[2].filePath).toMatch(/.+test\.yml$/);
+          expect(messages[2].range).toBeDefined();
+          expect(messages[2].range.length).toBeDefined();
+          expect(messages[2].range.length).toEqual(2);
+          expect(messages[2].range).toEqual([[0, 0], [0, 32]]);
         });
       });
     });
